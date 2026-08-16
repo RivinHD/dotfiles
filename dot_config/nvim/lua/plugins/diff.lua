9904e014aeca3c342363ce268dd0e2f77f427ee4
Vincent Gerlach <58261670+RivinHD@users.noreply.github.com>
2026-08-16 15:31:53 +0200

    feat: diffview keymaps

--- dot_config/nvim/lua/plugins/diff.lua
+++ dot_config/nvim/lua/plugins/diff.lua
@@ -1,5 +1,28 @@
+local function toggle_diffview(cmd)
+	if next(require("diffview.lib").views) == nil then
+		vim.cmd(cmd)
+	else
+		vim.cmd("DiffviewClose")
+	end
+end
+
 return {
 	{
 		"sindrets/diffview.nvim",
+		command = "DiffviewOpen",
+		keys = {
+			{
+				"<Leader>dff",
+				function()
+					toggle_diffview("DiffviewOpen")
+				end,
+			},
+			{
+				"<Leader>dfh",
+				function()
+					toggle_diffview("DiffviewFileHistory")
+				end,
+			},
+		},
 	},
 }
